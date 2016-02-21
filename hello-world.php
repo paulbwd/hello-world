b47de3a3c3b51299e3fa3d6599f03ea1ba6b8f97
Paul Brown <paulbwd@gmail.com>
2016-02-21 17:06:38 +0000

    Added files via upload
    Simple hello world script in PHP / HTML5

---
+++ hello-world.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php $helloWorld = ucwords("hello world"); ?>
+    <?php echo $helloWorld; ?>
+</body>
+</html>
